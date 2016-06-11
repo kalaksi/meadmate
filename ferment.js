2c1530b80faa052885b82538c430ffa264a81927
Kalaksi <kalaksi@users.noreply.github.com>
2016-06-11 19:38:11 +0300

    Some basic style improvements

--- ferment.js
+++ ferment.js
@@ -1,3 +1,17 @@
+// Copyright (C) 2016 kalaksi@users.noreply.github.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // per mol
 var MASS = {
@@ -52,7 +66,7 @@ function update_result_box(ethanol_l, water_l, result_field) {
         progress_bar_class = 'progress-bar-danger';
         percentage_message = 'mead-booze with ' + percentage.toFixed(1) + ' % of alcohol and DEAD YEAST';
     }
-    else if (percentage > 8.0) {
+    else if (percentage > 9.0) {
         progress_bar_class = 'progress-bar-warning';
         percentage_message = 'pretty strong but fine mead with ' + percentage.toFixed(1) + ' % of alcohol';
     }
